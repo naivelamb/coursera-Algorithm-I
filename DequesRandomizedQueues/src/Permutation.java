c1470abc5ab6357e74ec51ce45d50f9e8ca7679a
Xuan Cao <davidcao1991@gmail.com>
2017-04-03 11:06:12 -0400

    Add files via upload

--- DequesRandomizedQueues/src/Permutation.java
+++ DequesRandomizedQueues/src/Permutation.java
@@ -1,8 +1,17 @@
-import java.util.Iterator;
-import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 
 public class Permutation {
 	public static void main(String[] args) {
+		RandomizedQueue<String> rq = new RandomizedQueue<String>();
+		int k = Integer.parseInt(args[0]);
+		while(!StdIn.isEmpty()) {
+			String s = StdIn.readString();
+			rq.enqueue(s);
+		}
 		
+		for (int i = 0; i < k; i++) {
+			StdOut.println(rq.dequeue());
+		}
 	}
 }
