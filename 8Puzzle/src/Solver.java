48e3669b290d6a65a8adb521d8b508fd41817ad3
Xuan Cao <davidcao1991@gmail.com>
2017-04-12 17:19:29 -0400

    Add files via upload

---
+++ 8Puzzle/src/Solver.java
@@ -0,0 +1,24 @@
+
+public class Solver {
+	Board initial;
+	
+	public Solver(Board initial) {
+		this.initial = initial;
+	}
+	
+	public boolean isSolvable() {
+		return false;
+	}
+	
+	public int moves() {
+		return 0;
+	}
+	
+	public Iterable<Board> solution() {
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
